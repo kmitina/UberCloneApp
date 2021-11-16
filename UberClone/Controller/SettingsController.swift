161976a27aee3510853f0f45de6986eef44c200c
Kris Mitina <kristmitina@gmail.com>
2021-11-16 16:21:02 +0100

    Settings page UI & populate with user

---
+++ UberClone/Controller/SettingsController.swift
@@ -0,0 +1,64 @@
+//
+//  SettingsController.swift
+//  UberClone
+//
+//  Created by DVKSH on 16.11.21.
+//
+
+import UIKit
+
+private let reuseIdentifier = "LocationCell"
+
+class SettingsController: UITableViewController {
+    // MARK: - Properties
+    
+    private let user: User
+    
+    private lazy var infoHeader: UserInfoHeader = {
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 100)
+        let view = UserInfoHeader(user: user, frame: frame)
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTableView()
+        configureNavigationBar()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleDismissal() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureTableView() {
+        tableView.rowHeight = 60
+        tableView.register(LocationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.backgroundColor = .white
+        tableView.tableHeaderView = infoHeader
+    }
+    
+    func configureNavigationBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barStyle = .black
+        navigationItem.title = "Settings"
+        navigationController?.navigationBar.backgroundColor = .black
+        navigationController?.navigationBar.barTintColor = .backgroundColor
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "baseline_clear_white_36pt_2x").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleDismissal))
+    }
+}
--- UberClone/Controller/SideMenu/ContainerController.swift
+++ UberClone/Controller/SideMenu/ContainerController.swift
@@ -165,7 +165,10 @@ extension ContainerController: MenuControllerDelegate {
             case .yourTrips:
                 break
             case .settings:
-                break
+                guard let user = self.user else { return }
+                let controller = SettingsController(user: user)
+                let nav = UINavigationController(rootViewController: controller)
+                self.present(nav, animated: true, completion: nil)
             case .logout:
                 let alert = UIAlertController(title: nil, message: "Are you sure you want to log out?", preferredStyle: .actionSheet)
                 alert.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { _ in
